66d734dab8bc3a4d0202cda937c3bff4bfe55c95
Taneem <taneemhasan17@gmail.com>
2026-06-27 17:18:54 +0600

    Done enough request

--- prac_scrap/request_l.py
+++ prac_scrap/request_l.py
@@ -1,4 +1,19 @@
-import request
+import requests
 url = "https://xkcd.com/353/"
-r = request.get(url)
-print(url)
+image = "https://imgs.xkcd.com/comics/python.png"
+image_r = requests.get(image)
+# print(image_r.content)
+# with open('/home/x-xenon/Desktop/comic.png', "wb") as f:
+#     f.write(image_r.content)
+
+# print(image_r.status_code)
+# print(image_r.ok)
+
+# print(image_r.headers)
+
+# print(requests.get("https://httpbin.org/").headers)
+
+payload = {'username': 'taneem', 'password': 'testing'}
+test = "https://httpbun.com/basic-auth/taneem/testing"
+r = requests.get(test, auth=('taneem', 'testig'))
+print(r)
